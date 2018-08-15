0d57908ba9fc3ccc5e45f868dd15c14149d2bea3
Kamesas <semakaleksandr2014@gmail.com>
2018-08-15 18:53:21 +0300

    Добавил таксономии год, актер, страна

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@
 			//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
 			'hierarchical'        => false,
 			'supports'            => array('title','editor','thumbnail'), // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
-			'taxonomies'          => array('Genres'),
+			'taxonomies'          => array('Genres', 'yers', 'Сountry', 'actor'),
 			'has_archive'         => false,
 			'rewrite'             => true,
 			'query_var'           => true,
@@ -45,8 +45,8 @@
 
 
 // хук для регистрации
-add_action('init', 'create_taxonomy');
-function create_taxonomy(){
+add_action('init', 'create_taxonomy_genres');
+function create_taxonomy_genres(){
 	// список параметров: http://wp-kama.ru/function/get_taxonomy_labels
 	register_taxonomy('Genres', array('movie'), array(
 		'label'                 => '', // определяется параметром $labels->name
@@ -85,4 +85,101 @@ function create_taxonomy(){
 	) );
 }
 
+add_action('init', 'create_taxonomy_сountry');
+function create_taxonomy_сountry(){	
+	register_taxonomy('Сountry', array('movie'), array(
+		'label'                 => '',
+		'labels'                => array(
+			'name'              => 'Страны',
+			'singular_name'     => 'Страна',
+			'search_items'      => 'Найти страну',
+			'all_items'         => 'Все страны',
+			'view_item '        => 'Смотреть страну',
+			'parent_item'       => 'Родительская страна',
+			'parent_item_colon' => 'Родительская страна:',
+			'edit_item'         => 'Редактировать страну',
+			'update_item'       => 'Обновить страну',
+			'add_new_item'      => 'Добавить новую страну',
+			'new_item_name'     => 'Новое имя страны',
+			'menu_name'         => 'Страны',
+		),
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		'publicly_queryable'    => null, // равен аргументу public
+		'show_in_nav_menus'     => true, // равен аргументу public
+		'show_ui'               => true, // равен аргументу public
+		'show_in_menu'          => true, // равен аргументу show_ui
+		'show_tagcloud'         => true, // равен аргументу show_ui
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null, // $taxonomy
+		'hierarchical'          => false,
+		'update_count_callback' => '',
+		'rewrite'               => true,
+	) );
+}
 
+add_action('init', 'create_taxonomy_yers');
+function create_taxonomy_yers(){	
+	register_taxonomy('yers', array('movie'), array(
+		'label'                 => '',
+		'labels'                => array(
+			'name'              => 'Год',
+			'singular_name'     => 'Год',
+			'search_items'      => 'Найти год',
+			'all_items'         => 'Все года',
+			'view_item '        => 'Смотреть год',
+			'parent_item'       => '',
+			'parent_item_colon' => '',
+			'edit_item'         => 'Редактировать год',
+			'update_item'       => 'Обновить год',
+			'add_new_item'      => 'Добавить новый год',
+			'new_item_name'     => 'Новоый год',
+			'menu_name'         => 'Года',
+		),
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		'publicly_queryable'    => null, // равен аргументу public
+		'show_in_nav_menus'     => true, // равен аргументу public
+		'show_ui'               => true, // равен аргументу public
+		'show_in_menu'          => true, // равен аргументу show_ui
+		'show_tagcloud'         => true, // равен аргументу show_ui
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null, // $taxonomy
+		'hierarchical'          => false,
+		'update_count_callback' => '',
+		'rewrite'               => true,
+	) );
+}
+
+add_action('init', 'create_taxonomy_actor');
+function create_taxonomy_actor(){	
+	register_taxonomy('actor', array('movie'), array(
+		'label'                 => '',
+		'labels'                => array(
+			'name'              => 'Актеры',
+			'singular_name'     => 'Актер',
+			'search_items'      => 'Найти актера',
+			'all_items'         => 'Все актеры',
+			'view_item '        => 'Смотреть актера',
+			'parent_item'       => 'Родиетльский... ',
+			'parent_item_colon' => 'Родительский...',
+			'edit_item'         => 'Редактировать актера',
+			'update_item'       => 'Обновить актера',
+			'add_new_item'      => 'Добавить нового актера',
+			'new_item_name'     => 'Новый актер',
+			'menu_name'         => 'Актеры',
+		),
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		'publicly_queryable'    => null, // равен аргументу public
+		'show_in_nav_menus'     => true, // равен аргументу public
+		'show_ui'               => true, // равен аргументу public
+		'show_in_menu'          => true, // равен аргументу show_ui
+		'show_tagcloud'         => true, // равен аргументу show_ui
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null, // $taxonomy
+		'hierarchical'          => false,
+		'update_count_callback' => '',
+		'rewrite'               => true,
+	) );
+}
--- single-movie.php
+++ single-movie.php
@@ -5,7 +5,7 @@
 			
 	    <?php while ( have_posts() ) : the_post(); ?>
 				
-				<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				<h1><?php the_title(); ?></h1>
 				<?php the_terms( get_the_ID(), 'Genres', 'Жанры: ', '/', "." ); ?>
 		    <?php the_content() ?>
 		    <?php the_post_thumbnail(); ?>				
